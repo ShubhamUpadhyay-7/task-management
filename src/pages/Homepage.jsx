b9d8741f851d302e66a22179c86fe9ea2cfca693
Shubham Upadhyay <shubham.1822en1142@kiet.edu>
2024-11-02 15:53:24 +0530

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Homepage.module.css';
+
+const Homepage = () => {
+    return (
+        <div>
+            <div className={styles.left}>
+                <div className={styles.img1}>
+                    <img src="/assets/Group.png" alt="homepage_logo" />
+                </div>
+                <div className={styles.para}>
+                    <h1>Welcome aboard my friend</h1>
+                    <span>just a couple of clicks and we start</span>
+                </div>
+            </div>
+            <div className={styles.right}>
+                <div className={ }>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
